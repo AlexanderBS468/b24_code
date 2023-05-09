641106802478b514e93086ed2b3e79232426bbe9
AlexBS <alex.sad.468@gmail.com>
2023-05-09 10:20:02 +0100

    Added a lost language phrase

--- local/activities/custom/changesaleorderstatus/lang/ru/changesaleorderstatus.php
+++ local/activities/custom/changesaleorderstatus/lang/ru/changesaleorderstatus.php
@@ -3,3 +3,4 @@
 $MESS ['CRM_SOAD_ORDER_NOT_FOUND'] = "Не найден заказ у сделки";
 $MESS ['CRM_SOAD_ORDER_PROPERTY_STATUS_EMPTY'] = "Не установлено значение установки статуса заказа";
 $MESS ['CRM_SOAD_ORDER_STATUS_EXIST'] = "Заказ уже имеет такой статус";
+$MESS ['CRM_SOAD_ORDER_ERROR'] = "Текущий документ не является документом типа \"Заказ\"";
